7fcfdfab3c7356be64b66bdf5e2bc0fd62675fab
Leandro Araujo <leandro.a.santos@hotmail.com>
2021-10-21 21:41:43 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { useContext } from 'react';
+import styles from './App.module.scss';
+import { LoginBox } from './Components/LoginBox';
+import { MessageList } from './Components/MessageList';
+import { SendMessageForm } from './Components/SendMessageForm';
+import { AuthContext } from './contexts/auth';
+
+export function App() {
+
+  const { user } = useContext(AuthContext)
+
+  return (
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      { !!user ? <SendMessageForm /> : <LoginBox />}
+    </main>
+  )
+}
+
